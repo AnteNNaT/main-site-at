6c208f314aa8fb92ee824f28984fe89835b10eb9
Anna Talankina <anuta_t@mail.ru>
2020-05-25 16:36:37 +0500

    Урок 3. Исходный код.

--- src/main/java/ru/geekbrains/main/site/Actions.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.main.site;
-
-public interface Actions {
-
-      String running(float Distance);
-       String jumping(float Height);
-       String action(Obstacles obstacle);
-       boolean possibility(Obstacles obstacle);
-}
