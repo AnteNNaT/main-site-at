46066644fc0823dc6b159d3ccf36f2ae86731517
Anna Talankina <anuta_t@mail.ru>
2020-05-30 03:17:03 +0500

    Урок 4. ДЗ. Navigation Test готов.

--- src/test/java/ru/geekbrains/main/site/at/NavigationTest.java
+++ src/test/java/ru/geekbrains/main/site/at/NavigationTest.java
@@ -2,66 +2,52 @@ package ru.geekbrains.main.site.at;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import ru.geekbrains.main.site.at.base.BaseTest;
 
 public class NavigationTest extends BaseTest {
 
+    String getItemLinkName (String navItemName){
+        switch (navItemName){
+            case "Курсы" :{
+                return  "courses";
+            }
+            case "Вебинары" :{
+                return "events";
+            }
+            case "Форум" :{
+                return "topics";
+            }
+            case "Блог" :{
+                return "posts";
+            }
+            case "Тесты" :{
+                return "tests";
+            }
+            case "Карьера" :{
+                return "career";
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + navItemName);
+        }
+    }
 
-    void navigationTest(String selector, String headerText){
-        WebElement buttonCourses = BaseTest.getDriver().findElement(By.cssSelector(selector));
+    @ParameterizedTest
+    @ValueSource(strings = { "Курсы", "Вебинары", "Форум", "Блог", "Тесты", "Карьера"})
+    void navigationTest(String navItemName){
+        WebElement buttonCourses = BaseTest.getDriver().findElement(By.cssSelector("[id='nav'] [href='/"+
+                getItemLinkName (navItemName)+"']"));
         buttonCourses.click();
         WebElement header=BaseTest.getDriver().findElement(By.cssSelector("header[id='top-menu']"));
         Assertions.assertTrue(header.isDisplayed());
         WebElement textNamePage = BaseTest.getDriver().findElement(By.cssSelector("h2[class=\"gb-header__title\"]"));
-        Assertions.assertEquals(headerText, textNamePage.getText());
+        Assertions.assertEquals(navItemName, textNamePage.getText());
         WebElement footer=BaseTest.getDriver().findElement(By.cssSelector("footer[class='site-footer']"));
         Assertions.assertTrue(footer.isDisplayed());
     }
-
-    @Test
-    void courses() {
-
-        navigationTest("[id='nav'] [href='/courses']", "Курсы");
-    }
-
-    @Test
-    void events() {
-
-        navigationTest("[id='nav'] [href='/events']", "Вебинары");
-
-    }
-
-    @Test
-    void topics() {
-
-        navigationTest("[id='nav'] [href='/topics']", "Форум");
-
-    }
-
-    @Test
-    void posts() {
-
-        navigationTest("[id='nav'] [href='/posts']", "Блог");
-
-
-    }
-
-    @Test
-    void tests() {
-
-        navigationTest("[id='nav'] [href='/tests']", "Тесты");
-
-    }
-
-    @Test
-    void career() {
-
-        navigationTest("[id='nav'] [href='/career']", "Карьера");
-
-    }
-
 
 
 }
